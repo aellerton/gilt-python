7bceced3e0f0d2a02ea607a45fe2aad1d8702104
Andrew Ellerton <aellerton@gilt.com>
2012-02-29 20:57:51 -0500

    nojira: minor tweaks

--- samples/list-active-sales.py
+++ samples/list-active-sales.py
@@ -3,13 +3,10 @@ from gilt.rest import GiltApiClient
 
 for sale in GiltApiClient().sales.active():
   print "%s:" % sale.name
-  #if sale.description: print "  %s" % sale.description[0:80]
-  #if sale.products: print "  %d products" % len(sale.products)
   print "  Store:    %s" % sale.store
   print "  Key:      %s" % sale.sale_key
-  #print "  Begins:   %s" % sale.begins
-  #print "  Ends:     %s" % sale.ends
-  #print "  Duration: %s" % (sale.ends-sale.begins)
+  print "  Begins:   %s" % sale.begins
+  print "  Visit:    %s" % sale.sale_url
   print "  For details:"
   print "    python samples/get-sale-detail.py %s %s" % (sale.store, sale.sale_key)
   print
